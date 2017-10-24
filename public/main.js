7abb01fc5729043c1d653083cf9672db4a4c2b32
Steve Holgado <steveholgado@gmail.com>
2017-10-24 23:53:06 +0100

    Minor code cleanup

--- public/main.js
+++ public/main.js
@@ -1,14 +1,14 @@
 $(function() {
-  var socket = io();
+  const socket = io()
 
   // Ask user for username and store in socket object
-  socket.username = prompt('Please enter a username:');
+  socket.username = prompt('Please enter a username:')
 
   // Add welcome message
-  $('#messages').append($('<li>').text('Welcome to the chat'));
+  $('#messages').append($('<li>').text('Welcome to the chat'))
 
   // Create disconnection observable
-  var disconnect$ = Rx.Observable.fromEvent(socket, 'disconnect');
+  const disconnect$ = Rx.Observable.fromEvent(socket, 'disconnect')
 
   // Listen for connections
   Rx.Observable
@@ -16,7 +16,7 @@ $(function() {
     .subscribe(() => {
 
       // Send username to server
-      socket.emit('save username', socket.username);
+      socket.emit('save username', socket.username)
 
       // Listen for form submit events
       Rx.Observable
@@ -26,48 +26,50 @@ $(function() {
         .map(() => $('#message-box').val())
         .subscribe(message => {
           $('#message-box').val('')
-          $('#messages').append($('<li>').text(socket.username + ': ' + message))
+          const element = $('<li>').text(socket.username + ': ' + message)
+          $('#messages').append(element)
           socket.emit('chat message', message)
-        });
+        })
 
       // Listen for messages and attach to DOM
       Rx.Observable
         .fromEvent(socket, 'chat message')
         .takeUntil(disconnect$)
         .subscribe(data => {
-          $('#messages').append($('<li>').text(data.from + ': ' + data.message));
-          window.scrollTo(0, document.body.scrollHeight);
-        });
+          const element = $('<li>').text(data.from + ': ' + data.message)
+          $('#messages').append(element)
+          window.scrollTo(0, document.body.scrollHeight)
+        })
 
       // Listen for all users and rebuild DOM
       Rx.Observable
         .fromEvent(socket, 'all users')
         .takeUntil(disconnect$)
         .subscribe(users => {
-          $('#users').html('');
-          $('#users').append($('<option>').val('everyone').text('everyone'));
+          $('#users').html('')
+          $('#users').append($('<option>').val('everyone').text('everyone'))
           users.forEach(user => {
-            $('#users').append($('<option>').val(user.id).text(user.username));
-          });
-        });
+            const element = $('<option>').val(user.id).text(user.username)
+            $('#users').append(element)
+          })
+        })
 
       // Listen for new users and add to DOM
       Rx.Observable
         .fromEvent(socket, 'new user')
         .takeUntil(disconnect$)
-        .subscribe(users => {
-          users.forEach(user => {
-            $('#users').append($('<option>').val(user.id).text(user.username));
-          });
-        });
+        .subscribe(user => {
+          const element = $('<option>').val(user.id).text(user.username)
+          $('#users').append(element)
+        })
 
       // Listen for disconnected users and remove from DOM
       Rx.Observable
         .fromEvent(socket, 'remove user')
         .takeUntil(disconnect$)
         .subscribe(id => {
-          $('#users option[value=' + id + ']').remove();
-        });
+          $('#users option[value=' + id + ']').remove()
+        })
 
-    });
-});
+    })
+})
--- server.js
+++ server.js
@@ -1,27 +1,26 @@
-const express = require('express');
-const app     = express();
-const http    = require('http').Server(app);
-const io      = require('socket.io')(http);
-const port    = process.env.PORT || 3000;
-const Rx      = require('rx');
+const express = require('express')
+const app     = express()
+const http    = require('http').Server(app)
+const io      = require('socket.io')(http)
+const port    = process.env.PORT || 3000
+const Rx      = require('rx')
 
 // Serve static files
 app.use(express.static('public'))
 
 // Routes
-app.get('/', function(req, res) {
-  res.sendFile('/index.html');
-});
+app.get('/', (req, res) => res.sendFile('/index.html'))
 
 // Get all users
-const getAllUsers = function() {
-  const allSockets = io.sockets.sockets;
+const getAllUsers = () => {
+  const allSockets = io.sockets.sockets
   return Object.keys(allSockets)
     .filter(key => allSockets[key].username)
-    .map(key => {
-      return { id: key, username: allSockets[key].username }
-    });
-};
+    .map(key => ({
+      id: key,
+      username: allSockets[key].username
+    }))
+}
 
 // Listen for connections
 Rx.Observable
@@ -29,10 +28,10 @@ Rx.Observable
   .subscribe(client => {
 
     // Create disconnection observable
-    const disconnect$ = Rx.Observable.fromEvent(client, 'disconnect');
+    const disconnect$ = Rx.Observable.fromEvent(client, 'disconnect')
 
     // Send array of all users
-    client.emit('all users', getAllUsers());
+    client.emit('all users', getAllUsers())
 
     // Listen for message events
     Rx.Observable
@@ -41,7 +40,7 @@ Rx.Observable
       .subscribe(message => client.broadcast.emit('chat message', {
         from: client.username,
         message: message
-      }));
+      }))
 
     // Check for new user and store username in socket object
     Rx.Observable
@@ -49,19 +48,17 @@ Rx.Observable
       .takeUntil(disconnect$)
       .do(username => io.sockets.sockets[client.id].username = username)
       .subscribe(username => {
-        client.broadcast.emit('new user', [{
+        client.broadcast.emit('new user', {
           id: client.id,
           username: username
-        }]);
-      });
+        })
+      })
 
     // On disconnect, tell other users
     disconnect$
-      .subscribe(() => client.broadcast.emit('remove user', client.id));
+      .subscribe(() => client.broadcast.emit('remove user', client.id))
     
-  });
+  })
 
 // Start app listening
-http.listen(port, function() {
-  console.log('listening on *:' + port);
-});
+http.listen(port, () => console.log('listening on *:' + port))
