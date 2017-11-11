996b2d54abf0596cffa789cea3fe10195234abfc
Steve Holgado <steveholgado@gmail.com>
2017-11-11 00:39:40 +0000

    Reorganised client code

--- public/main.js
+++ public/main.js
@@ -1,75 +1,120 @@
 $(function() {
-  const socket = io()
 
-  // Ask user for username and store in socket object
-  socket.username = prompt('Please enter a username:')
+  // Socket stream
+  const socket$ = Rx.Observable.of(io())
 
-  // Add welcome message
-  $('#messages').append($('<li>').text('Welcome to the chat'))
+  // Connection stream
+  const connection$ = socket$
+    .mergeMap(socket => Rx.Observable
+      .fromEvent(socket, 'connect')
+      .map(() => socket)
+    )
 
-  // Create disconnection observable
-  const disconnect$ = Rx.Observable.fromEvent(socket, 'disconnect')
+  // Disconnection stream
+  const disconnect$ = connection$
+    .mergeMap(socket => Rx.Observable
+      .fromEvent(socket, 'disconnect')
+      .map(() => socket)
+    )
 
-  // Listen for connections
-  Rx.Observable
-    .fromEvent(socket, 'connect')
-    .subscribe(() => {
+  // Form submit stream
+  const formSubmit$ = connection$
+    .mergeMap(socket => Rx.Observable
+      .fromEvent($('form'), 'submit')
+      .do(e => e.preventDefault())
+      .map(() => ({
+        socket: socket,
+        message: $('#message-box').val()
+      }))
+      .takeUntil(disconnect$)
+    )
 
-      // Send username to server
+  // Chat message stream
+  const chatMessage$ = connection$
+    .mergeMap(socket => Rx.Observable
+      .fromEvent(socket, 'chat message')
+      .takeUntil(disconnect$)
+    )
+
+  // All users stream
+  const allUsers$ = connection$
+    .mergeMap(socket => Rx.Observable
+      .fromEvent(socket, 'all users')
+      .takeUntil(disconnect$)
+    )
+
+  // New user stream
+  const newUser$ = connection$
+    .mergeMap(socket => Rx.Observable
+      .fromEvent(socket, 'new user')
+      .takeUntil(disconnect$)
+    )
+
+  // Remove user stream
+  const removeUser$ = connection$
+    .mergeMap(socket => Rx.Observable
+      .fromEvent(socket, 'remove user')
+      .takeUntil(disconnect$)
+    )
+
+  // On connection
+  connection$
+    .subscribe(socket => {
+      // Ask use for username
+      socket.username = prompt('Please enter a username:')
+
+      // If no username, generate random
+      if (!socket.username) {
+        const randomNum = Math.floor(Math.random * 1000)
+        socket.username = user + randomNum
+      }
+
+      // Tell server to store username in socket object
       socket.emit('save username', socket.username)
 
-      // Listen for form submit events
-      Rx.Observable
-        .fromEvent($('form'), 'submit')
-        .takeUntil(disconnect$)
-        .do(e => e.preventDefault())
-        .map(() => $('#message-box').val())
-        .subscribe(message => {
-          $('#message-box').val('')
-          const element = $('<li>').text(socket.username + ': ' + message)
-          $('#messages').append(element)
-          socket.emit('chat message', message)
-        })
-
-      // Listen for messages and attach to DOM
-      Rx.Observable
-        .fromEvent(socket, 'chat message')
-        .takeUntil(disconnect$)
-        .subscribe(data => {
-          const element = $('<li>').text(data.from + ': ' + data.message)
-          $('#messages').append(element)
-          window.scrollTo(0, document.body.scrollHeight)
-        })
-
-      // Listen for all users and rebuild DOM
-      Rx.Observable
-        .fromEvent(socket, 'all users')
-        .takeUntil(disconnect$)
-        .subscribe(users => {
-          $('#users').html('')
-          $('#users').append($('<option>').val('everyone').text('everyone'))
-          users.forEach(user => {
-            const element = $('<option>').val(user.id).text(user.username)
-            $('#users').append(element)
-          })
-        })
-
-      // Listen for new users and add to DOM
-      Rx.Observable
-        .fromEvent(socket, 'new user')
-        .takeUntil(disconnect$)
-        .subscribe(user => {
-          const element = $('<option>').val(user.id).text(user.username)
-          $('#users').append(element)
-        })
-
-      // Listen for disconnected users and remove from DOM
-      Rx.Observable
-        .fromEvent(socket, 'remove user')
-        .takeUntil(disconnect$)
-        .subscribe(id => {
-          $('#users option[value=' + id + ']').remove()
-        })
-
+      // Add welcome message
+      $('#messages').append($('<li>').text('Welcome to the chat'))
     })
+
+  // Listen for form submit events
+  formSubmit$
+    .subscribe(({ socket, message }) => {
+      $('#message-box').val('')
+      const element = $('<li>').text(socket.username + ': ' + message)
+      $('#messages').append(element)
+      socket.emit('chat message', message)
+    })
+
+  // Listen for messages and attach to DOM
+  chatMessage$
+    .subscribe(data => {
+      const element = $('<li>').text(data.from + ': ' + data.message)
+      $('#messages').append(element)
+      window.scrollTo(0, document.body.scrollHeight)
+    })
+
+  // Listen for all users and rebuild DOM
+  allUsers$
+    .subscribe(users => {
+      $('#users').html('')
+      $('#users').append($('<option>').val('everyone').text('everyone'))
+      users.forEach(user => {
+        const element = $('<option>').val(user.id).text(user.username)
+        $('#users').append(element)
+      })
+    })
+
+  // Listen for new users and add to DOM
+  newUser$
+    .subscribe(user => {
+      const element = $('<option>').val(user.id).text(user.username)
+      $('#users').append(element)
+    })
+
+  // Listen for disconnected users and remove from DOM
+  removeUser$
+    .subscribe(id => {
+      $('#users option[value=' + id + ']').remove()
+    })
+
 })
