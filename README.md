9ce4f133c43c09fe5787a806ff4eec0eab817dba
Steve Holgado <steveholgado@gmail.com>
2017-12-30 21:38:24 +0000

    Added webpack and README

---
+++ README.md
@@ -0,0 +1,20 @@
+# RxJS Chat App
+
+Chat app built with RxJS and Socket.io
+
+## Setup
+
+### Install dependencies:
+```
+npm install
+```
+
+### Build client:
+```
+npm run build
+```
+
+### Run server:
+```
+npm run server
+```
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.join(__dirname, 'public'),
+		filename: 'bundle.min.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['env']
+				}
+			}
+		]
+	}
+}
