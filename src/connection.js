d8027350d0db899891f0097b6216cf6b87f8a50a
Steve Holgado <steveholgado@gmail.com>
2018-01-03 22:29:37 +0000

    Wrapped socket in an observable

--- src/connection.js
+++ src/connection.js
@@ -1,18 +1,23 @@
 import Rx from 'rxjs'
 import io from 'socket.io-client'
 
-const socket = io()
+const socket$ = Rx.Observable.of(io())
 
-const connection$ = Rx.Observable.fromEvent(socket, 'connect')
-const disconnect$ = Rx.Observable.fromEvent(socket, 'disconnect')
+const connection$ = socket$
+	.mergeMap(socket => Rx.Observable.fromEvent(socket, 'connect').map(() => socket))
 
+const disconnect$ = socket$
+	.mergeMap(socket => Rx.Observable.fromEvent(socket, 'disconnect'))
+
+// On connection, emit data from observable
 const send = (observable, event) => connection$
-  .mergeMap(() => observable)
+  .mergeMap(socket => observable.map(data => ({ socket, data })))
   .takeUntil(disconnect$)
-  .subscribe(data => socket.emit(event, data))
+  .subscribe(({ socket, data }) => socket.emit(event, data))
 
+// On connection, listen for event
 const listen = (event) => connection$
-  .mergeMap(() => Rx.Observable.fromEvent(socket, event))
+  .mergeMap(socket => Rx.Observable.fromEvent(socket, event))
   .takeUntil(disconnect$)
 
 export default { send, listen }
