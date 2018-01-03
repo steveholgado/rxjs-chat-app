5ff079ed7587e9a64437fb6421f959273597c733
Steve Holgado <steveholgado@gmail.com>
2018-01-03 23:33:05 +0000

    Minor formatting of client code

--- src/connection.js
+++ src/connection.js
@@ -4,20 +4,29 @@ import io from 'socket.io-client'
 const socket$ = Rx.Observable.of(io())
 
 const connection$ = socket$
-	.mergeMap(socket => Rx.Observable.fromEvent(socket, 'connect').map(() => socket))
+	.mergeMap(socket => {
+		return Rx.Observable.fromEvent(socket, 'connect')
+			.map(() => socket)
+	})
 
 const disconnect$ = socket$
-	.mergeMap(socket => Rx.Observable.fromEvent(socket, 'disconnect'))
+	.mergeMap(socket => {
+		return Rx.Observable.fromEvent(socket, 'disconnect')
+	})
 
 // On connection, emit data from observable
-const send = (observable, event) => connection$
-  .mergeMap(socket => observable.map(data => ({ socket, data })))
-  .takeUntil(disconnect$)
-  .subscribe(({ socket, data }) => socket.emit(event, data))
+const send = (observable, event) => {
+	return connection$
+	  .mergeMap(socket => observable.map(data => ({ socket, data })))
+	  .takeUntil(disconnect$)
+	  .subscribe(({ socket, data }) => socket.emit(event, data))
+}
 
 // On connection, listen for event
-const listen = (event) => connection$
-  .mergeMap(socket => Rx.Observable.fromEvent(socket, event))
-  .takeUntil(disconnect$)
+const listen = (event) => {
+	return connection$
+	  .mergeMap(socket => Rx.Observable.fromEvent(socket, event))
+	  .takeUntil(disconnect$)
+}
 
 export default { send, listen }
