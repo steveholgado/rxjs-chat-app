bc307802213e31a2f7c4e587a410f7baa8a283d4
Steve Holgado <steveholgado@gmail.com>
2018-02-10 00:17:48 +0000

    Minor formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 const path = require('path')
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.min.js'
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				query: {
-					presets: ['env']
-				}
-			}
-		]
-	}
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.min.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env']
+        }
+      }
+    ]
+  }
 }
